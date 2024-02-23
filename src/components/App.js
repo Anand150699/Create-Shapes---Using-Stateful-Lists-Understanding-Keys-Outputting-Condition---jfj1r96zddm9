498bdc192181bed3f7b7e0e7cd5b23d06e45c0fb
Khandelwal <anandx.khandelwal@intel.com>
2024-02-24 04:30:33 +0530

    ok, it's done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-
+  const [shapes, setShapes]= useState([])
+  const[shaper, setShaper]=useState("square")
+  function handleClick(){
+    setShapes([...shapes, shaper.toLowerCase()])
+  }
   return (
     <div id="main">
       <div id="shape-creator">
-        
-
+        <select onChange={(e)=>setShaper(e.target.value)} value={shaper}>
+          <option>Square</option>
+          <option>Circle</option>
+        </select>
+        <button onClick={handleClick}>Add Shape</button>
       </div>
       <div id="shapes-holder">
+        {shapes.map((shape, index)=>(
+          <div className={shape} key={index}>{index}</div>
+        ))}
       </div>
     </div>
   )
